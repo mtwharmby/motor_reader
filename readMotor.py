14df11ccca664f2a1ee8de1df4cec4854019ef09
Michael Wharmby <mtwharmby@gmail.com>
2019-04-10 23:48:50 +0200

    Adds generate device names method and test.

--- readMotor.py
+++ readMotor.py
@@ -4,9 +4,27 @@
 ''' This is the list of parameters for ZMX and OMSvme respectively which will be read/written'''
 _parameters_list = {'zmx': [],
                     'oms': []}
+_beamline = 'p021'
+_servers = {'EH1A': 64, 'EH1B': 16}
 
 
-def read_parameters(zmx_dp, oms_dp):
+def generate_device_names(server, dev_ids=None, beamline=None):
+    if not beamline:
+        beamline = _beamline
+
+    if dev_ids:
+        if not isinstance(dev_ids, list):
+            dev_ids = [dev_ids]
+    else:
+        dev_ids = list(range(_servers[server]))
+
+    dev_names = {}
+    for i in dev_ids:
+        mot_name = '{}.{:02d}'.format(server, i)
+        oms_zmx_names = {'oms': '{}/motor/{}'.format(beamline, mot_name),
+                         'zmx': '{}/ZMX/{}'.format(beamline, mot_name)}
+        dev_names[mot_name] = oms_zmx_names
+    return dev_names
     '''
     Reads the motor parameters listed in _parameters_list by querying the specified zmx and oms Tango servers
     '''
--- test_read_motor.py
+++ test_read_motor.py
@@ -1,25 +1,44 @@
 import pytest
 from mock import Mock, patch
 
-from readMotor import read_parameters
+from readMotor import read_parameters, generate_device_names
 
 
 @pytest.fixture
 def patch_DevProxy(mocker):
+    PyTango = pytest.importorskip('PyTango')
     return mocker.patch("readMotor.DeviceProxy")
-
-
-def test_write_readable_table(mocker):
+#
+#
+# def test
+#
+#
+# def test_write_readable_table(mocker):
+#     pass
+#
+#
+def test_write_dat_file():
     pass
 
 
-def test_write_dat_file(mocker):
-    pass
+def test_generate_device_names():
+    # Name pattern for ZMX: {beamline}/ZMX/{server}.{nn}
+    # Name pattern for OMS: {beamline}/motor/{server}.{nn}
+    # When provided with b/l & server, return dictionary of names of devices
+    dev_names = generate_device_names('EH1A', [12], 'bl00')
+    assert dev_names == {'EH1A.12': {'oms': 'bl00/motor/EH1A.12',
+                                     'zmx': 'bl00/ZMX/EH1A.12'}}
+
+    dev_names = generate_device_names('EH1B', beamline='bl00')
+    assert len(dev_names) == 16
+    assert dev_names['EH1B.07'] == {'oms': 'bl00/motor/EH1B.07',
+                                    'zmx': 'bl00/ZMX/EH1B.07'}
 
 
 @patch('readMotor._parameters_list', {'zmx': ['ZMXParam1', 'ZMXParam2'],
                                       'oms': ['OMSParam3', 'OMSParam4']})
 def test_read_motor_parameters(mocker):
+    # monkeypatch.setattr(PyTango, 'DeviceProxy', Mock())
     # Parameters table should have form:
     # { 'zmx': [zmx_params], 'oms': [oms_params] }
     # - zmx_params should be requested from ZMX Tango
@@ -27,7 +46,7 @@ def test_read_motor_parameters(mocker):
     zmx_dp_mock = Mock()
     oms_dp_mock = Mock()
 
-    motor_dict = read_parameters(zmx_dp_mock, oms_dp_mock)
+    motor_dict = read_parameters(oms_dp_mock, zmx_dp_mock)
 
     zmx_dp_mock.read_attribute.assert_called_with('ZMXParam2')
     oms_dp_mock.read_attribute.assert_called_with('OMSParam4')
